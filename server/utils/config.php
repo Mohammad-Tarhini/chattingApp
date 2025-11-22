eae7ec1a51c01d16e4191d0bf923d21679adc288
mohamad baker tarhini <tarhinimohammadbaker@gmail.com>
2025-11-22 21:05:17 +0200

    create server

--- server/utils/config.php
+++ server/utils/config.php
@@ -1,5 +1,5 @@
 <?php
 define("MAX_Tokens" , 300);
 define("GPT_VER" , "gpt-3.5-turbo");
-define("OPEN_AI_KEY" , "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");
+define("OPEN_AI_KEY" , "");
 ?>
